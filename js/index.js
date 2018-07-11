f0b8678fc1072112c6b53078cb372e70cdd28191
Nick Boober <nboober@terpmail.umd.edu>
2018-07-11 12:59:06 -0400

    style: Added pathing in index.html and restaurant.html to index.js. Updated pathing in the open cache in index.js so the pathing is correct

--- js/index.js
+++ js/index.js
@@ -4,12 +4,20 @@ self.addEventListener('install', function(event){ //add event listener for the i
     caches.open('restaurant-reviews-v1').then(function(cache){  //Creates/Opens a new cache
       return cache.addAll(  //Anything I want the cache to store will be put in here
         '/',
-        'styles/styles.css',
-        'images/1.jpg',
-        'scripts/dbhelper.js',
-        'scripts/main.js',
-        'scripts/restaurant_info.js'
-
+        'css/styles.css',
+        'img/1.png',
+        'img/2.png',
+        'img/3.png',
+        'img/4.png',
+        'img/5.png',
+        'img/6.png',
+        'img/7.png',
+        'img/8.png',
+        'img/9.png',
+        'img/10.png',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js'
       );
     })
   );
