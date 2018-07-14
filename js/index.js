19dc2dc3f2dd944711ebde6779e81e56f64882d8
Nick Boober <nboober@terpmail.umd.edu>
2018-07-14 13:27:44 -0400

    feat: Updated js files to allow for offlineFirst c[3~[Coompatability

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
+let staticCacheName = 'restaurant-reviews-v1';
+
 //Register the service worker
 if (navigator.serviceWorker){ //Browser support detection for service Worker
-  navigator.serviceWorker.register('/index.js', {
+  navigator.serviceWorker.register('./index.js', {
     scope: '/localhost'
   }).then(function(reg){
     console.log("Service Worker Registered");
@@ -9,31 +11,51 @@ if (navigator.serviceWorker){ //Browser support detection for service Worker
   });
 }
 
-self.addEventListener('fetch', function(event){
-  console.log(event.request);
-});
-
 //Installing the service worker
 self.addEventListener('install', function(event){ //add event listener for the install event
   event.waitUntil(  //Signals the progress of the install
-    caches.open('restaurant-reviews-v1').then(function(cache){  //Creates/Opens a new cache
+    caches.open(staticCacheName).then(function(cache){  //Creates/Opens a new cache
       return cache.addAll(  //Anything I want the cache to store will be put in here
-        '/',
-        'css/styles.css',
-        'img/1.png',
-        'img/2.png',
-        'img/3.png',
-        'img/4.png',
-        'img/5.png',
-        'img/6.png',
-        'img/7.png',
-        'img/8.png',
-        'img/9.png',
-        'img/10.png',
-        'js/dbhelper.js',
-        'js/main.js',
-        'js/restaurant_info.js'
+        './',
+        '../css/styles.css',
+        '../img/1.png',
+        '../img/2.png',
+        '../img/3.png',
+        '../img/4.png',
+        '../img/5.png',
+        '../img/6.png',
+        '../img/7.png',
+        '../img/8.png',
+        '../img/9.png',
+        '../img/10.png',
+        './js/dbhelper.js',
+        './js/main.js',
+        './js/restaurant_info.js'
       );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+        caches.match(event.request).then(function(response){
+          if (response) return response;
+          return fetch(event.request);
+        });
+      );
+    });
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all{
+      cacheNames.filter(function(cacheNames){
+        return cacheName.startsWith('restaurant') &&
+              cacheName != staticCacheName;
+            }).map(function(cacheName){
+              return cache.delete(cacheName);
+        });
+      })
     })
   );
 });
--- js/indexController.js
+++ js/indexController.js
@@ -1,11 +1,37 @@
+import restaurants from '../data/restaurants.json';
+import reviews from '../data/restaurants.json';
+
 export default function IndexController(container){
+  this._container = container;
+  this._restaurants = new restaurant(this._container);
+  this._reviews = new review(this._container);
+  this._lostConnectionToast = null;
+  this._openSocket();
   this._registerServiceWorker();
 }
+
+// open a connection to the server for live updates
+IndexController.prototype._openSocket = function() {
+  var indexController = this;
+  var latestPostDate = this._postsView.getLatestPostDate();
+
+  ws.addEventListener('message', function(event) {
+    requestAnimationFrame(function() {
+      indexController._onSocketMessage(event.data);
+    });
+  });
+
+  // called when the web socket sends message data
+  IndexController.prototype._onSocketMessage = function(data) {
+    var messages = JSON.parse(data);
+    this._restaurants.addPosts(messages);
+    this._reviews.addPosts(messages);
+  };
 
 IndexController.prototype._registerServiceWorker = function() { //Service Worker implementor
 
   if(!navigator.serviceWorker) return;
-  navigator.serviceWorker.register('/index.js').then(function(){
+  navigator.serviceWorker.register('./index.js').then(function(){
     console.log("Registration Worked");
   }).catch(function(){
     console.log("Registration Failed");
